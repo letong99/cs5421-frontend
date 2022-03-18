b9003ed8b2c08f075cf2f66034cec27c689e8987
stanleyseow173 <seowszehowestanley@gmail.com>
2022-03-19 00:34:48 +0800

    init

--- src/appRouter.js
+++ src/appRouter.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import { ChallengeDetail } from './pages';
+import UserProfile from './pages/userProfile/UserProfile.js';
+import ChallengesBoard from './pages/challengesBoard/ChallengesBoard.js';
+
 
 const AppRouter = (props) => {
     return (
         <Switch>
-            <Route path="/details" component={ChallengeDetail}/>
+            <Route path="/details" component={ChallengeDetail}/> 
+            <Route path="/profile" component={UserProfile}/>
+            <Route path="/challenges" component={ChallengesBoard}/>
         </Switch>
     )
 }
