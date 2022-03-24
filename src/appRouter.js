127a42f6e44eb52c5f0d0aa2bca5dfab46d51c9e
Sveta151 <46090129+Sveta151@users.noreply.github.com>
2022-03-24 22:45:22 +0800

    add createChallenge file and add it to the rooting

--- src/appRouter.js
+++ src/appRouter.js
@@ -4,6 +4,7 @@ import { ChallengeDetail } from './pages';
 import UserProfile from './pages/userProfile/UserProfile.js';
 import ChallengesBoard from './pages/challengesBoard/ChallengesBoard.js';
 import { Login } from './pages';
+import ChallengeCreate from './pages/challengeCreate/ChallengeCreate.js';
 
 
 const AppRouter = (props) => {
@@ -13,6 +14,7 @@ const AppRouter = (props) => {
             <Route path="/profile" component={UserProfile}/>
             <Route path="/challenges" component={ChallengesBoard}/>
             <Route path="/login" component={Login}/>
+            <Route path='/create' component = {ChallengeCreate}/>
         </Switch>
     )
 }
