166cc18cf5c643552cb806bb4997432212c46849
letong99 <wei538664@gmail.com>
2022-04-02 20:34:59 +0800

    persist context

--- src/components/CurrentUserContext.js
+++ src/components/CurrentUserContext.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const CurrentUserContext = React.createContext();
 
 export const CurrentUserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = React.useState(null);
-  const [currentUserRole, setCurrentUserRole] = React.useState(null);
+  const [currentUser, setCurrentUser] = React.useState(
+    localStorage.getItem("user")
+  );
+  const [currentUserRole, setCurrentUserRole] = React.useState(
+    localStorage.getItem("role")
+  );
 
   const pushCurrentUser = (user_id, role) => {
     setCurrentUser(user_id);
@@ -15,6 +19,11 @@ export const CurrentUserProvider = ({ children }) => {
     setCurrentUser(null);
     setCurrentUserRole(null);
   };
+
+  useEffect(() => {
+    localStorage.setItem("user", currentUser);
+    localStorage.setItem("role", currentUserRole);
+  }, [currentUserRole, currentUser]);
 
   return (
     <CurrentUserContext.Provider
